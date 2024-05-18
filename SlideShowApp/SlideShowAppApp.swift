1d01601dedbf35f9f48a35d5d7522ffbf2203f91
gzonelee <gzonelee@gmail.com>
2024-05-18 14:40:36 +0900

    Initial Commit

---
+++ SlideShowApp/SlideShowAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  SlideShowAppApp.swift
+//  SlideShowApp
+//
+//  Created by gzonelee on 5/18/24.
+//
+
+import SwiftUI
+
+@main
+struct SlideShowAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
